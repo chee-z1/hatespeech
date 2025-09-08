45ed7566d2cbdd019c66ee2e55619046010745b8
11115박서우 <yellowPolyp@proton.me>
2025-09-08 18:30:16 +0900

    인공지능 분류 기능을 추가.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from os import lseek
 import time
 import json
+import csv
 
 import httpx
 from bs4 import BeautifulSoup
+from tqdm.auto import tqdm
+
+from transformers import TextClassificationPipeline, BertForSequenceClassification, AutoTokenizer
 
 class DCBestFetcher:
     requests = 0
@@ -81,8 +85,74 @@ class DCBestFetcher:
         self.get_post_urls()
         for url in self.url_list:
             self.add_post_content(url)
-        file = open(file_name, 'w')
-        file.write(json.dumps(self.posts, ensure_ascii=False))
+        with open(file_name, 'w') as file:
+            file.write(json.dumps(self.posts, ensure_ascii=False))
+            file.close()
 
-fetcher = DCBestFetcher()
-fetcher.save_posts_json("./posts.json")
+class JsonPostClassifier:
+    posts = []
+    csv_result = {}
+    json_result = []
+
+    def __init__(self, file_name):
+        file = open(file_name, 'r')
+        contents = file.read()
+        self.posts = json.loads(contents)
+
+    def classify_posts(self):
+        texts = []
+        for post in self.posts:
+            texts.append(post['title'] + '\n' + post['content'])
+            texts.extend(post['comments'])
+        print('Successfully loaded JSON')
+        print(f'{len(texts)} data found')
+
+        model_name = 'smilegate-ai/kor_unsmile'
+        model = BertForSequenceClassification.from_pretrained(model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        pipe = TextClassificationPipeline(
+                model = model,
+                tokenizer = tokenizer,
+                padding = True,
+                truncation = True,
+                device=1,
+                batch_size = 8,
+                function_to_apply = 'sigmoid'
+                )
+
+        nth = 0
+        for result in tqdm(pipe(texts)):
+            print(texts[nth])
+            print(result)
+            if not result['label'] in self.csv_result:
+                self.csv_result[result['label']] = 1
+            else:
+                self.csv_result[result['label']] += 1
+            self.json_result.append(
+                    {'text': texts[nth], 'label': result['label']}
+                    )
+            nth += 1
+
+    def write_classified_csv(self, file_name):
+        with open(file_name, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['라벨', '수량', '비율(%)'])
+            for key,val in self.csv_result.items():
+                writer.writerow([key, val, f'{val/len(self.json_result)}%'])
+            writer.writerow(['전체', len(self.json_result), '100%'])
+            file.close()
+
+    def write_classified_json(self, file_name):
+        with open(file_name, 'w') as file:
+            file.write(json.dumps(self.json_result, ensure_ascii=False))
+            file.close()
+
+
+
+#fetcher = DCBestFetcher()
+#fetcher.save_posts_json("./posts.json")
+classifier = JsonPostClassifier("./posts.json")
+classifier.classify_posts()
+print(classifier.csv_result)
+classifier.write_classified_csv("./result.csv")
+classifier.write_classified_json("./result.json")
